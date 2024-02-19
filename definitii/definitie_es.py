2b05259b37abc6b7be89007907e2da5b84294d98
amun311 <123638470+amun311@users.noreply.github.com>
2024-02-19 10:10:39 +0000

    test roolback

--- definitii/definitie_es.py
+++ definitii/definitie_es.py
@@ -4,21 +4,21 @@ from bs4 import BeautifulSoup
 def definitie_es(cuv):
     user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
     headers = { 'User-Agent' : user_agent }
-  
-    req = urllib.Request(f'https://dle.rae.es/{cuv}', None, headers)
-    response = urllib.urlopen(req)
-    page = response.read()
-    soup = BeautifulSoup(page.decode(), 'html.parser')
-    #print(soup)
-    response.close() 
-    #except: soup = ''
+    try:
+      req = urllib.Request(f'https://dle.rae.es/{cuv}', None, headers)
+      response = urllib.urlopen(req)
+      page = response.read()
+      soup = BeautifulSoup(page.decode(), 'html.parser')
+      #print(soup)
+      response.close() 
+    except: soup = ''
     pt = r'1. (\w{1,}. \w*\s*.*).\",'
     definitie = re.search(pt,str(soup))
     #definitie = str(definitie)
     try:
         definitie= definitie.group(1)
     except:
-        definitie = '...'
+        definitie = ''
     dfn = str(definitie).lstrip("['").rstrip("']")
     
     return dfn
